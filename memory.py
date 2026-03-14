bdc206a52d0494c1a3499f8663162864f804d046
Abdullah Mansoor <abdulla.3t5@gmail.com>
2026-03-14 16:43:46 +0400

    abdullah Updated datapath and memory

--- memory.py
+++ memory.py
@@ -1,32 +1,29 @@
 # Abdulsalam And Abdullah
 class MipsMemory:
     def __init__(self):
-        # We use a dictionary to store memory values. 
-        # The key is the address and the value is the 32-bit word.
+       # memory storage: address -> value
         self.storage = {}
 
     def load_program(self, start_address, instructions): 
-        # Loads the user's assembly program into memory.
-        # Instructions should be passed as a list of integers.
+        # load instructions into memory
         current_address = start_address
         for instr in instructions:
             self.storage[current_address] = instr
-            current_address += 4 
+            current_address += 4   # load instructions into memory
 
     def load_initial_data(self, address, value):  
-        # Loads specific data items into memory before simulation starts.
+         # store data in memory before execution
         self.storage[address] = value
 
     def read(self, address):
-        
-        # Reads a word from memory. Used by instruction fetch and 'lw'.
-        # If address doesn't exist, we return 0 as a default.
+        # read value from memory (used by lw)
+        # if address not found, return 0
         return self.storage.get(address, 0)
 
     def write(self, address, value):
-        # Writes a word to memory. Used by the 'sw'.
+       # write value to memory (used by sw)
         self.storage[address] = value
 
     def get_all_data(self):   
-        # Returns the full memory dictionary. 
+        # return all memory content 
         return self.storage
